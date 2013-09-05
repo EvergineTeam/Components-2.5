ef5c8802168860284470841f8d64e3c08d1010db
WaveEngine Team <waveengineteam@gmail.com>
2013-09-05 16:04:00 +0200

    Add catalog and resources

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+#if WINDOWS
+[assembly: AssemblyTitle("WaveEngine.Components")]
+#elif ANDROID
+[assembly: AssemblyTitle("WaveEngineAndroid.Components")]
+#elif IOS
+[assembly: AssemblyTitle("WaveEngineiOS.Components")]
+#elif METRO
+[assembly: AssemblyTitle("WaveEngineMetro.Components")]
+#elif WINDOWS_PHONE7
+[assembly: AssemblyTitle("WaveEngineWP.Components")]
+#endif
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wave Corporation")]
+[assembly: AssemblyCopyright("Copyright © Wave Corporation 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d4161402-ce18-494c-9f3a-cde368b1f964")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
---
+++ Resources/StaticResources.cs
@@ -0,0 +1,82 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// StaticResources
+//
+// Copyright © 2010 - 2013 Wave Coorporation. All rights reserved.
+// Use is subject to license terms.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Services; 
+#endregion
+
+namespace WaveEngine.Components.Resources
+{
+    /// <summary>
+    /// Access to Embebed resources
+    /// </summary>
+    public static class StaticResources
+    {
+        /// <summary>
+        /// Default font path (embebed resource)
+        /// </summary>
+#if WINDOWS
+        private static readonly string defaultFontResourcePath = "WaveEngine.Components.Resources.DefaultFont.wpk";
+#endif
+#if WINDOWS_PHONE7
+        private static readonly string defaultFontResourcePath = "WaveEngineWP7.Components.Resources.DefaultFont.wpk";
+#endif
+#if WINDOWS_PHONE
+        private static readonly string defaultFontResourcePath = "WaveEngineWP.Components.Resources.DefaultFont.wpk";
+#endif
+#if METRO
+        private static readonly string defaultFontResourcePath = "WaveEngineMetro.Components.Resources.DefaultFont.wpk";
+#endif
+#if ANDROID
+        private static readonly string defaultFontResourcePath = "WaveEngineAndroid.Components.Resources.DefaultFont.wpk";
+#endif
+#if IOS
+        private static readonly string defaultFontResourcePath = "WaveEngineiOS.Components.Resources.DefaultFont.wpk";
+#endif
+
+        /// <summary>
+        /// The default sprite font
+        /// </summary>
+        private static SpriteFont defaultSpriteFont;
+
+        /// <summary>
+        /// Gets the default sprite font.
+        /// </summary>
+        /// <value>
+        /// The default sprite font.
+        /// </value>
+        public static SpriteFont DefaultSpriteFont
+        {
+            get
+            {
+                if (defaultSpriteFont == null)
+                {
+                    Stream defaultFontStream = null;
+#if METRO || PSSuite
+                    Assembly assembly = typeof(StaticResources).GetTypeInfo().Assembly;
+                    defaultFontStream = assembly.GetManifestResourceStream(defaultFontResourcePath);
+#else
+                    System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+                    defaultFontStream = assembly.GetManifestResourceStream(defaultFontResourcePath);
+#endif
+                    defaultSpriteFont = WaveServices.Assets.Global.LoadAsset<SpriteFont>(defaultFontResourcePath, defaultFontStream);
+                }
+
+                return defaultSpriteFont;
+            }
+        }
+    }
+}
